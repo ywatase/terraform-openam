36a735e1a6d1936866dce57b65bdd22ebb4730fa
Yusuke Watase <ywatase@gmail.com>
2016-06-27 20:55:07 +0900

    add maintenance_ip

--- security_group.tf
+++ security_group.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "internal" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.public_ip}/32"]
+    cidr_blocks = ["${var.maintenance_ip}/32"]
   }
 
   # Outbound internet connection
